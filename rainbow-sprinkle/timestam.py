8dd5132b071a52c839268b9e0891b5421e4d6ef3
aronswar <harung1993@gmail.com>
2018-08-22 14:40:56 -0400

    added module for top 2

--- rainbow-sprinkle/timestam.py
+++ rainbow-sprinkle/timestam.py
@@ -15,6 +15,9 @@ def get_value_and_timestamp(user_id):
     else:
         print("user already exist")
 
+#method to allocate the key as per the requirements. the results are multiplied
+#by -1 since the Heapq library uses min binary heap and we need max binary heap.
+
 def set_priority_val(ID_user):
     for key in ID_user:
         if key%3==0 and key%5==0:
@@ -29,9 +32,15 @@ def set_priority_val(ID_user):
         else:
             new_value1=key*(-1)
             heapq.heappush(q,(new_value1,key,"Normal"))
-    print(new_ls)
+
+#module to calculate the amount of time the job was in the Queue
+def time_of_wait(id_value):
+    time_of_entry=new_dic[id_value]
+    time_now_after=time.time()
+    time_wait_final=time_now_after-time_of_entry
+    return(time_wait_final)
+
+#module to find the top most job
+def top_most_item():
     next_item=heapq.heappop(q)
     print(next_item)
-
-
-set_priority_val(new_dict_vla)
